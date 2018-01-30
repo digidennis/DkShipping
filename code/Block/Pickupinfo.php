edb93a899beceac1160633a738709542cd3b04e8
Dennis Jessen <digidennis3@gmail.com>
2018-01-30 23:13:01 +0100

    pickup

--- code/Block/Pickupinfo.php
+++ code/Block/Pickupinfo.php
@@ -8,16 +8,24 @@ class Digidennis_DkShipping_Block_Pickupinfo extends Mage_Core_Block_Template
         $this->setTemplate('digidennis/dkshipping/pickupinfo.phtml');
     }
 
-    public function getAddress($order)
+    public function getAddress()
     {
-        if( $order->getShippingMethod() == 'dkshipping_pickup_taastrup' )
+        if( $this->getOrder()->getShippingMethod() == 'dkshipping_pickup_taastrup' )
         {
             return "Roskildvej 332A<br/>2630 Taastrup<br/>Hverdage 10:00-16:00";
         }
-        elseif ($order->getShippingMethod() == 'dkshipping_pickup_ganloese' )
+        elseif ( $this->getOrder()->getShippingMethod() == 'dkshipping_pickup_ganloese' )
         {
             return "Ringbakken 14<br/>3660 Stenløse<br/>man-tor 8:00-15:00<br/>fre 8:00-12:00";
         }
         return '';
+    }
+
+    public function hasPickup($order)
+    {
+        if( $this->getOrder()->getShippingMethod() == 'dkshipping_pickup_taastrup' ||
+            $this->getOrder()->getShippingMethod() == 'dkshipping_pickup_ganloese' )
+            return true;
+        return false;
     }
 }
